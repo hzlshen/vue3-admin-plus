afd9da1f3df63164e5589660b77bdd63e344ac5e
hzlshen <m15277968105@163.com>
2022-04-20 17:56:41 +0800

    add style

--- src/App.vue
+++ src/App.vue
@@ -8,12 +8,5 @@
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "./style/main.css";
 </style>
