a7ff2e11b83c75b1a983f80d491e35d395f93c0a
hzlshen <m15277968105@163.com>
2022-04-22 23:01:17 +0800

    table vue3

--- src/components/template/hzlTable.vue
+++ src/components/template/hzlTable.vue
@@ -1,67 +1,51 @@
 <template>
   <div>
-    <el-table 
+    <el-table
       border
       :data="DataList"
       highlight-current-row
       :height="height"
       @row-click="handleSelectedRow"
-      @selection-change="handleSelectionChange">
+      @selection-change="handleSelectionChange"
+    >
       <el-table-column
-        v-for="item in cols" 
+        v-for="item in cols"
         :key="item.prop"
         :label="item.label"
         :prop="item.prop"
         :width="item.width"
         :type="item.type"
-        >
+      >
         <!-- :formatter="formatter" -->
         <!-- formatter的用法：需要在每个col里添加formatter,push方法进去 对数据进行格式化 -->
       </el-table-column>
-    </el-table>  
-  </div>  
+    </el-table>
+  </div>
 </template>
-<script>
-import moment from 'moment'
-export default {
-  props: {
-    DataList: {
-      type: Array,
-      default() {
-        return []
-      }
-    },
-    cols: {
-      type: Array,
-      default() {
-        return []
-      }
-    },
-    height: {
-      type: [Array,Number],
-      default: '410'
-    }
-  },
-  data() {
-    return {
-    }
-  },
-  mounted() {
-  },
-  watch: {
-  },
-  methods: {
-    handleSelectedRow() {
+<script setup>
+// import moment from "moment";
+import { defineProps } from "vue";
 
+const props = defineProps({
+  DataList: {
+    type: Array,
+    default() {
+      return [];
     },
-    handleSelectionChange() {
+  },
+  cols: {
+    type: Array,
+    default() {
+      return [];
     },
-    // formatter(row) {
-    //   console.log(row);
-    // }
-  }
-}
+  },
+  height: {
+    type: [Array, Number],
+    default: "410",
+  },
+});
+
+const handleSelectedRow = () => {};
+const handleSelectionChange = () => {};
 </script>
-<style>
-
-</style>
+<style></style>
