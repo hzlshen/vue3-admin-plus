dbc98a72f8f27fc2877574c2a92ab82301c89314
hzlshen <m15277968105@163.com>
2022-04-22 15:39:19 +0800

    add sidebar

--- src/components/sidebar/sidebar.vue
+++ src/components/sidebar/sidebar.vue
@@ -9,9 +9,9 @@
       unique-opened
       router
     >
-      <template v-for="item in sidebarInfo">
-        <el-submenu :index="item.index" :key="item.index" v-if="item.subNav">
-          <template slot="title">
+      <template v-for="item in dataMap.sidebarInfo">
+        <el-sub-menu :index="item.index" :key="item.index" v-if="item.subNav">
+          <template #title>
             <i :class="item.icon"></i>
             <span>{{ item.title }}</span>
           </template>
@@ -23,11 +23,11 @@
               :key="list.index"
             >{{ list.title }}</el-menu-item>
           </el-menu-item-group>
-        </el-submenu>
+        </el-sub-menu>
         <el-menu-item :index="item.index" :key="item.index" v-else>
-          <template>
+          <template #title>
             <i :class="item.icon"></i>
-            <span slot="title">{{ item.title }}</span>
+            <span>{{ item.title }}</span>
           </template>
         </el-menu-item>
       </template>
@@ -35,128 +35,124 @@
   </div>
 </template>
 
-<script>
-import { mapState, mapActions } from "vuex";
-export default {
-  name: "sidebar",
-  data() {
-    return {
-      indexPath: [], //保存点击过的页面路由信息
-      sidebarInfo: [
-        //左侧导航条信息
+<script setup>
+import i18n from "@/locales/index";
+import { reactive } from "vue";
+
+const t = i18n.global.t;
+// const menuHome = { name: t('menus.home') }
+
+const dataMap = reactive({
+  indexPath: [], //保存点击过的页面路由信息
+  sidebarInfo: [
+    //左侧导航条信息
+    {
+      icon: "el-icon-setting",
+      index: "chart",
+      title: t("account")
+    },
+    /**
+     * inquiry 原始单据
+     * **/
+    {
+      icon: "el-icon-tickets",
+      index: "1",
+      title: t("invoice"),
+      subNav: [
         {
-          icon: "el-icon-setting",
-          index: "chart",
-          title: this.$t("account")
-        },
-        /**
-         * inquiry 原始单据
-         * **/
-        {
-          icon: "el-icon-tickets",
-          index: "1",
-          title: this.$t("invoice"),
-          subNav: [
-            {
-              index: "/orderReceivable",
-              title: "应收单据"
-            },
-            {
-              index: "/transactionFlow",
-              title: "交易流水"
-            },
-            {
-              index: "/systemStatement",
-              title: "系统对账单"
-            },
-            {
-              index: "/bankFlow",
-              title: "银行流水"
-            }
-          ]
-        },
-        /**
-         * verifyPlatform 对账平台
-         * **/
-        {
-          icon: "el-icon-date",
-          index: "2",
-          title: this.$t("reconciliation"),
-          subNav: [
-            {
-              index: "/verifyReceivables",
-              title: "收款对账"
-            },
-            {
-              index: "/verifyAutomate",
-              title: "自动对账"
-            },
-            {
-              index: "/verifyBankStatement",
-              title: "银行对账"
-            }
-          ]
-        },
-        /**
-         * verifyRecord 对账记录
-         * **/
-        {
-          icon: "el-icon-goods",
-          title: this.$t("record"),
-          index: "3",
-          subNav: [
-            {
-              index: "/verifyReceivablesRecord",
-              title: "收款记录"
-            },
-            {
-              index: "/verifyBankStatementRecord",
-              title: "银行记录"
-            }
-          ]
-        },
-        /**
-         * reportForms报表
-         * **/
-        {
-          icon: "el-icon-star-off",
-          title: this.$t("report"),
-          index: "4",
-          subNav: [
-            {
-              index: "/OrderSummary",
-              title: "订单执行总表"
-            },
-            {
-              index: "/ReceivablesBalance",
-              title: "收款对账余额表"
-            }
-          ]
+          index: "/orderReceivable",
+          title: "应收单据"
         },
         {
-          icon: "el-icon-more",
-          title: this.$t("setting"),
-          index: "5",
-          subNav: [
-            {
-              index: "/Structure",
-              title: "平台管理"
-            },
-            {
-              index: "/UpdatePWD",
-              title: "修改密码"
-            }
-          ]
+          index: "/transactionFlow",
+          title: "交易流水"
+        },
+        {
+          index: "/systemStatement",
+          title: "系统对账单"
+        },
+        {
+          index: "/bankFlow",
+          title: "银行流水"
         }
       ]
-    };
-  },
-  computed: {
-    ...mapState({
-      collapse: state => state.collapse
-    })
-  },
-  methods: {}
-};
+    },
+    /**
+     * verifyPlatform 对账平台
+     * **/
+    {
+      icon: "el-icon-date",
+      index: "2",
+      title: t("reconciliation"),
+      subNav: [
+        {
+          index: "/verifyReceivables",
+          title: "收款对账"
+        },
+        {
+          index: "/verifyAutomate",
+          title: "自动对账"
+        },
+        {
+          index: "/verifyBankStatement",
+          title: "银行对账"
+        }
+      ]
+    },
+    /**
+     * verifyRecord 对账记录
+     * **/
+    {
+      icon: "el-icon-goods",
+      title: t("record"),
+      index: "3",
+      subNav: [
+        {
+          index: "/verifyReceivablesRecord",
+          title: "收款记录"
+        },
+        {
+          index: "/verifyBankStatementRecord",
+          title: "银行记录"
+        }
+      ]
+    },
+    /**
+     * reportForms报表
+     * **/
+    {
+      icon: "el-icon-star-off",
+      title: t("report"),
+      index: "4",
+      subNav: [
+        {
+          index: "/OrderSummary",
+          title: "订单执行总表"
+        },
+        {
+          index: "/ReceivablesBalance",
+          title: "收款对账余额表"
+        }
+      ]
+    },
+    {
+      icon: "el-icon-more",
+      title: t("setting"),
+      index: "5",
+      subNav: [
+        {
+          index: "/Structure",
+          title: "平台管理"
+        },
+        {
+          index: "/UpdatePWD",
+          title: "修改密码"
+        }
+      ]
+    }
+  ]
+});
 </script>
+
+
 <style scoped></style>
