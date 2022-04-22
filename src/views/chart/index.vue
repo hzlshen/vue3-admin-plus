712358562e201b9bb196ce14492bdcdebf6efa55
hzlshen <m15277968105@163.com>
2022-04-22 13:49:35 +0800

    add echart

--- src/views/chart/index.vue
+++ src/views/chart/index.vue
@@ -1,15 +1,17 @@
 <template>
-  <el-tabs v-model="activeName" @tab-click="handleClick">
-    <el-tab-pane label="用户管理" name="first">
-      <div id="chartId" style="width: 100%; height: 300px"></div>
-    </el-tab-pane>
-    <el-tab-pane label="配置管理" name="second">
-      <!--  <div id="chartId" style="width: 100%; height: 300px"></div>-->
-    </el-tab-pane>
-    <el-tab-pane label="角色管理" name="third">
-      <!--  <div id="chartId" style="width: 100%; height: 300px"></div>-->
-    </el-tab-pane>
-  </el-tabs>
+  <div class="admin-echarts">
+    <el-tabs v-model="activeName" @tab-click="handleClick">
+      <el-tab-pane label="用户管理" name="first">
+        <div id="chartId" style="width: 100%; height: 300px"></div>
+      </el-tab-pane>
+      <el-tab-pane label="配置管理" name="second">
+        <!--  <div id="chartId" style="width: 100%; height: 300px"></div>-->
+      </el-tab-pane>
+      <el-tab-pane label="角色管理" name="third">
+        <!--  <div id="chartId" style="width: 100%; height: 300px"></div>-->
+      </el-tab-pane>
+    </el-tabs>
+  </div>
 </template>
 
 <script>
@@ -19,7 +21,7 @@ export default {
   name: "chart",
   setup(props) {
     const dataMap = reactive({
-      activeName: "first",
+      activeName: "first"
     });
 
     const handleClick = (tab, event) => {
@@ -33,52 +35,56 @@ export default {
       // 绘制图表
       myChart.setOption({
         title: {
-          text: "今日话务统计",
+          text: "今日话务统计"
         },
         tooltip: {
           trigger: "axis",
           axisPointer: {
-            type: "shadow",
-          },
+            type: "shadow"
+          }
         },
         grid: {
           left: "3%",
           right: "4%",
           bottom: "3%",
-          containLabel: true,
+          containLabel: true
         },
         xAxis: [
           {
             type: "category",
             data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
             axisTick: {
-              alignWithLabel: true,
-            },
-          },
+              alignWithLabel: true
+            }
+          }
         ],
         yAxis: [
           {
-            type: "value",
-          },
+            type: "value"
+          }
         ],
         series: [
           {
             name: "直接访问",
             type: "bar",
             barWidth: "60%",
-            data: [10, 52, 200, 334, 390, 330, 220],
-          },
-        ],
+            data: [10, 52, 200, 334, 390, 330, 220]
+          }
+        ]
       });
-      window.onresize = function () {
+      window.onresize = function() {
         myChart.resize();
       };
     });
 
     return {
-      ...dataMap,
+      ...dataMap
     };
-  },
+  }
 };
 </script>
-<style scoped></style>
+<style scoped>
+.admin-echarts {
+  margin: 20px auto;
+}
+</style>
