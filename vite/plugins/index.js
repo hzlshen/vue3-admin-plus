10a200ecef2dea73bd14fd4673ecbecb284af8e9
hzlshen <m15277968105@163.com>
2025-03-19 21:50:12 +0800

    add vite 配置code spliting 如 element plus

--- vite/plugins/index.js
+++ vite/plugins/index.js
@@ -7,6 +7,8 @@ import createSetupExtend from './setup-extend'
 import {
     viteMockServe
 } from 'vite-plugin-mock'
+import chunkSplitPlugin from 'vite-plugin-chunk-split';
+
 
 export default function createVitePlugins(viteEnv, isBuild = false) {
     const vitePlugins = [vue()]
@@ -15,6 +17,12 @@ export default function createVitePlugins(viteEnv, isBuild = false) {
     vitePlugins.push(createSvgIcon(isBuild))
     vitePlugins.push(viteMockServe({
         supportTs: true
+    }))
+    vitePlugins.push(chunkSplitPlugin({
+        customSplitting: {
+        // 自定义分割逻辑，例如基于文件或模块名称进行分割
+            element: ['element-plus']
+        }
     }))
     isBuild && vitePlugins.push(...createCompression(viteEnv))
     return vitePlugins
